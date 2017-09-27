16e07bd983d5d28d609777dba4b28c9f37c5180f
Nguyễn Chí Thành <chithanh.ptit@gmail.com>
2017-09-27 15:59:25 +0700

    321
    321

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var Config = {
   "mysql_host":   '127.0.0.1',
   "mysql_user":   'root',
-  "mysql_pass":   'Smartconnect89',
+  "mysql_pass":   'aKen2002',
   "mysql_data":   'chatapp_database',
-  "app_ip"	  :   '10.148.0.2',
+  "app_ip"	  :   '10.140.0.2',
   "app_port"  :   '8080',
   "secret"	  :   '@!@aKen2002@!@',
   "secretAdmin"	  :   '@!@aKen2002Admin@!@'
